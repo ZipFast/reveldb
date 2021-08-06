9cc4758f0d31600550de88a2694b0199295e68b7
zhangwenjum <2780898229@qq.com>
2021-08-06 11:30:36 +0800

    add compare enum K

--- src/random.rs
+++ src/random.rs
@@ -1,32 +1,37 @@
+use std::cell::Cell;
 pub struct Random {
-    seed: u32,
+    seed: Cell<u32>,
 }
 
 impl Random {
     pub fn new(seed: u32) -> Self {
-        Self { seed }
+        Self {
+            seed: Cell::new(seed),
+        }
     }
 
-    pub fn next(&mut self) -> ui32 {
+    pub fn next(&self) -> u32 {
         let M: u32 = 2147483647;
         let A: u64 = 16807;
-        let product = self.seed * M;
-        self.seed = ((product >> 31) + (product & M)) as u32;
-        if self.seed > M {
-            self.seed -= M;
-        }
+        let product = self.seed.get() as u64 * A;
         self.seed
+            .set(((product >> 31) as u32 + (product as u32 & M)) as u32);
+        let _seed = self.seed.get();
+        if _seed > M {
+            self.seed.set(_seed - M);
+        }
+        self.seed.get()
     }
 
-    pub fn uniform(&mut self, n: i32) -> u32 {
+    pub fn uniform(&self, n: u32) -> u32 {
         self.next() % n
     }
 
-    pub fn one_in(&mut self, n: i32) -> bool {
+    pub fn one_in(&self, n: u32) -> bool {
         (self.next() % n) == 0
     }
 
-    pub fn skewed(&mut self, max_log: i32) -> u32 {
+    pub fn skewed(&self, max_log: u32) -> u32 {
         self.uniform(1 << self.uniform(max_log + 1))
     }
 }
--- src/skiplist.rs
+++ src/skiplist.rs
@@ -1,155 +1,83 @@
-use rand::Rng;
-use std::{
-    ptr::{null, null_mut},
-    sync::atomic::{AtomicPtr, AtomicUsize, Ordering},
-    vec,
-};
-const KMaxHeight: usize = 12;
-pub struct SkipList {
-    head_: *mut Node,
-    max_hegiht_: AtomicUsize,
+use crossbeam::atomic::AtomicCell;
+use std::cmp::Ordering;
+use std::convert::TryInto;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+
+use crate::random::Random;
+const K_MAX_HEIGHT: u32 = 8;
+
+#[derive(Eq)]
+enum K<T: Ord> {
+    Minimum,
+    Key(T),
 }
 
-struct Node {
-    next_: Vec<AtomicPtr<Node>>,
-    key_: Vec<u8>,
-}
-
-impl Node {
-    fn new(key: Vec<u8>, height: usize) -> Self {
-        let mut vec = Vec::new();
-        for i in 0..height {
-            vec.push(AtomicPtr::new(null_mut()));
-        }
-        Self {
-            next_: vec,
-            key_: key,
-        }
-    }
-    fn Next(&self, level: usize) -> *mut Node {
-        assert!(level >= 0);
-        self.next_[level].load(Ordering::Acquire)
-    }
-
-    fn SetNext(&self, level: usize, next: *mut Node) {
-        assert!(level >= 0);
-        self.next_[level].store(next, Ordering::Release)
-    }
-}
-
-impl SkipList {
-    fn new() -> Self {
-        Self {
-            head_: Box::into_raw(Box::new(Node::new(vec![0], KMaxHeight))),
-            max_hegiht_: AtomicUsize::new(1),
-        }
-    }
-
-    fn GetMaxHeight(&self) -> usize {
-        self.max_hegiht_.load(Ordering::Relaxed)
-    }
-
-    fn keyIsAfterNode(key: &Vec<u8>, n: *mut Node) -> bool {
-        !n.is_null() && unsafe { key.to_vec() > n.as_ref().unwrap().key_ }
-    }
-
-    fn findGreaterOrEqual(&self, key: &Vec<u8>, prev: &mut Vec<*mut Node>) -> *mut Node {
-        let mut n = self.head_;
-        let mut level = self.GetMaxHeight() - 1;
-        loop {
-            unsafe {
-                let next = n.as_ref().unwrap().Next(level);
-                if SkipList::keyIsAfterNode(key, next) {
-                    n = next;
-                } else {
-                    if prev.len() != 0 {
-                        prev[level] = n;
-                    }
-                    if level == 0 {
-                        return next;
-                    } else {
-                        level -= 1;
-                    }
-                }
-            }
-        }
-    }
-
-    fn findLessThan(&self, key: &Vec<u8>) -> *mut Node {
-        let mut n = self.head_;
-        let mut level = self.GetMaxHeight() - 1;
-        loop {
-            unsafe {
-                let next = n.as_ref().unwrap().Next(level);
-                if next == null_mut() || next.as_ref().unwrap().key_ > key.to_vec() {
-                    if level == 0 {
-                        return n;
-                    } else {
-                        level -= 1;
-                    }
-                } else {
-                    n = next;
-                }
-            }
-        }
-    }
-
-    fn GetRandomHeight() -> usize {
-        let mut height = 1;
-        let kBranching = 4;
-        while height < KMaxHeight && (rand::random::<usize>() % kBranching) == 0 {
-            height += 1;
-        }
-        height
-    }
-
-    fn Insert(&mut self, key: &Vec<u8>) {
-        let mut prev = vec![null_mut(); KMaxHeight];
-        let n = self.findGreaterOrEqual(key, &mut prev);
-        let height = Self::GetRandomHeight();
-        if height > self.GetMaxHeight() {
-            for i in self.GetMaxHeight()..height {
-                prev[i] = self.head_;
-            }
-            self.max_hegiht_.store(height, Ordering::Relaxed);
-        }
-        let x = Box::into_raw(Box::new(Node::new(key.to_vec(), height)));
-        for i in 0..height {
-            unsafe {
-                x.as_mut()
-                    .unwrap()
-                    .SetNext(i, prev[i].as_mut().unwrap().Next(i));
-                prev[i].as_mut().unwrap().SetNext(i, x);
-            }
-        }
-    }
-
-    fn contain(&self, key: &Vec<u8>) -> bool {
-        let mut prev = vec![];
-        let n = self.findGreaterOrEqual(key, &mut prev);
-        println!("key is {:?}, node is {:?}", key, unsafe {});
-        if n != null_mut() && unsafe { key.to_vec() == n.as_ref().unwrap().key_ } {
-            true
-        } else {
-            false
+impl<T: Ord> Ord for K<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self {
+            &K::Minimum => match other {
+                &K::Minimum => Ordering::Equal,
+                &K::Key(ref t) => Ordering::Less,
+            },
+            &K::Key(ref x) => match other {
+                &K::Minimum => Ordering::Greater,
+                &K::Key(ref y) => x.cmp(y),
+            },
         }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T: Ord> PartialOrd for K<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: Ord> PartialEq for K<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.eq(other)
+    }
+}
+
+pub struct Node<T: Ord> {
+    key: T,
+    next: Vec<AtomicCell<Option<NonNull<Node<T>>>>>,
+}
+
+impl<T: Ord> Node<T> {
+    fn new(key: T, height: u32) -> Self {
+        let mut ret = Self {
+            key,
+            next: Vec::with_capacity(height as usize),
+        };
+        for _ in 0..height {
+            ret.next.push(AtomicCell::default());
+        }
+        ret
+    }
+
+    fn next(&self, level: u32) -> Option<NonNull<Node<T>>> {
+        self.next[level as usize].load()
+    }
+
+    fn set_next(&self, level: u32, x: Option<NonNull<Node<T>>>) {
+        self.next[level as usize].store(x)
+    }
+}
+
+mod test {
+    use crate::skiplist::K;
+    use std::cmp::Ordering;
     #[test]
-    fn test_empty() {
-        let mut list = SkipList::new();
-        list.Insert(&"hello".as_bytes().to_vec());
-        assert_eq!(list.contain(&"hello".as_bytes().to_vec()), true);
-    }
-
-    #[test]
-    fn test_insert_and_lookup() {
-        let N: i32 = 2000;
-        let R: i32 = 5000;
-        rand
+    fn test_key() {
+        let Minimum1 = K::<i32>::Minimum;
+        let Minimum2 = K::<i32>::Minimum;
+        assert_eq!(Minimum1.cmp(&Minimum2), Ordering::Equal);
+        let x = K::Key(12);
+        let y = K::Key(1);
+        assert_eq!(Minimum1.cmp(&x), Ordering::Less);
+        assert_eq!(x.cmp(&Minimum1), Ordering::Greater);
+        assert_eq!(x.cmp(&y), Ordering::Greater);
     }
 }
